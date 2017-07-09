8be6f03d085a1f6e97352c741eed9a53234e94d9
Kya <kyajmil@gmail.com>
2017-07-09 16:40:05 -0400

    Initial commit

---
+++ altmer/GenAltmerNames.py
@@ -0,0 +1,2 @@
+femalePrefixes = ['Andra', 'Ca', 'Cymba', 'Kori', 'Lili', 'Lora', 'Mor', 'Psy', 'Saur', 'Sha', 'Valli', 'Zeno']
+femaleSuffixes = ['ginia', 'lia', 'lina', 'mia', 'na', 'sara', 'sephona', 'sha', 'tha']
